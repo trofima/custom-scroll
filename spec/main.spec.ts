da5616729825331fe4193b16aa74d2cc894887a6
trofima <azarija@gmail.com>
2016-06-20 12:07:39 +0300

    init

---
+++ spec/main.spec.ts
@@ -0,0 +1,14 @@
+'use strict';
+
+import {CustomScroll} from './../src/scripts/main';
+
+describe(`Class CustomScroll.`, function() {
+    beforeEach(function() {
+    });
+
+    describe(``, function() {
+        it(`should `, function() {
+
+        });
+    });
+});
---
+++ src/scripts/main.ts
@@ -0,0 +1,7 @@
+'use strict';
+
+export class CustomScroll {
+    constructor() {
+        
+    }
+}
