b0e2637794d57a86c98521f3b44519488dbcc1bb
trofima <azarija@gmail.com>
2016-06-20 23:49:27 +0300

    mocks and test driver created, first test implemented

--- spec/main.spec.ts
+++ spec/main.spec.ts
@@ -2,13 +2,132 @@
 
 import {CustomScroll} from './../src/scripts/main';
 
+class ElementMock {
+    offsetHeight = 0;
+    offsetTop = 0;
+    scrollHeight = 0;
+    children = [];
+
+    constructor(props) {
+        // super();
+
+        Object.keys(props).forEach((key) => this[key] = props[key]);
+    }
+
+    querySelector(selector) {
+        if (selector.charAt(0) === '.')
+            for (let child of this.children) {
+                if (child.classList.contains(selector.substring(1)))
+                    return child;
+            }
+
+        return null;
+    }
+
+    executeMutation(){}
+
+
+    // core imitation
+
+    private classes = [];
+
+    classList = {
+        add: (className) => {
+            if (this.classes.indexOf(className) === -1)
+                this.classes.push(className);
+        },
+
+        contains: (className) => this.classes.indexOf(className) > -1,
+    };
+
+    style = {
+        setProperty: (name, value) => {
+            if (name === 'height')
+                this.offsetHeight = parseInt(value, 10);
+
+            this.style[name] = value;
+        }
+    };
+}
+
+class DOMObserverMock {
+    private callback = null;
+    private node = null;
+
+    constructor(callback) {
+        this.callback = callback;
+    }
+
+    observe(node) {
+        this.node = node;
+
+        this.node.executeMutation = () => this.callback();
+    }
+}
+
+class CustomScrollDriver {
+    customScroll;
+
+    constructor() {}
+
+    modules = {
+        DOMObserver: DOMObserverMock
+    };
+
+    nodes = {
+        el: null,
+        shifted: null,
+        content: null,
+        bar: null,
+        thumb: null,
+    };
+
+    when = {
+        instantiated: () => this.customScroll = new CustomScroll(
+            this.nodes.el,
+            // DocumentMock,
+            this.modules.DOMObserver
+        ),
+
+        nodes: (nodes) => {
+            this.node('content', nodes.content);
+            this.node('shifted', nodes.shifted, [this.nodes.content]);
+            this.node('thumb', nodes.thumb);
+            this.node('bar', nodes.bar, [this.nodes.thumb]);
+            this.node('el', nodes.el, [this.nodes.shifted, this.nodes.bar]);
+
+            return this;
+        }
+    };
+
+    private node(name, params, children = []) {
+        var node = this.nodes[name] = new ElementMock(params);
+
+        node.children = children;
+        node.classList.add(name);
+    }
+}
+
 describe(`Class CustomScroll.`, function() {
     beforeEach(function() {
+        this.driver = new CustomScrollDriver();
+
+        this.driver
+            .when.nodes({
+                el: {offsetHeight: 100},
+                shifted: {scrollHeight: 200},
+                content: {scrollHeight: 200},
+                bar: {},
+                thumb: {offsetHeight: 100},
+            })
+            .when.instantiated();
     });
 
-    describe(``, function() {
-        it(`should `, function() {
+    describe(`constructor:`, function() {
+        it(`should calculate scroll thumb height`, function() {
+            this.driver.nodes.content.executeMutation();
 
+            expect(this.driver.nodes.thumb.offsetHeight).toBe(50);
         });
     });
 });
--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,7 +1,151 @@
 'use strict';
 
+interface MutationObserverClass {
+    new (callback:MutationCallback):MutationObserver;
+}
+
 export class CustomScroll {
-    constructor() {
+    //TODO: fix infinite scroll
+    //TODO: set to body cursor: pointer while draging thumb
+    //TODO: on window resize
+    //TODO: display scroll handle on scrolling (if window is not focused, animation doesn't show the handle)
+    //TODO: on bar click scroll ?
+
+    private nodes = {
+        el: null,
+        shifted: null,
+        content: null,
+        bar: null,
+        thumb: null,
+    };
+
+    private document = {
+        el: null,
+        listeners: []
+    };
+
+    private scroll = {
+        thumbY: 0,
+        callbacks: [],
+    };
+
+    constructor(
+        element:HTMLElement,
+        // documentEl:Document,
+        DOMObserver
+    ) {
+        // this.document.el = documentEl;
         
+        // console.log(element.offsetHeight, element);
+
+        this.setNodes(element);
+        this.observeDOMMutation(DOMObserver);
+        // this.addEventListeners();
+    }
+
+    addScrollListener(callback) {
+        this.scroll.callbacks.push(callback);
+    }
+
+    private setNodes(el) {
+        this.nodes.el = el;
+        this.nodes.shifted = el.querySelector('.shifted');
+        this.nodes.content = this.nodes.shifted.querySelector('.content');
+        this.nodes.bar = el.querySelector('.bar');
+        this.nodes.thumb = this.nodes.bar.querySelector('.thumb');
+    }
+
+    private observeDOMMutation(DOMObserver) {
+        let domObserver = new DOMObserver(() => this.update());
+
+        domObserver.observe(this.nodes.content, {
+            childList: true,
+            attributes: true,
+            subtree: true
+        });
+    }
+
+    private update() {
+        if (this.nodes.shifted.scrollHeight > this.nodes.el.offsetHeight) {
+            this.show();
+        } else {
+            this.hide();
+        }
+    }
+
+    private hide() {
+        this.nodes.el.classList.remove('visible');
+    }
+
+    private show() {
+        let thumbHeight = Math.pow(this.nodes.el.offsetHeight, 2)
+            / this.nodes.shifted.scrollHeight + 'px';
+
+        this.nodes.el.classList.add('visible');
+        this.nodes.thumb.style.setProperty('height', thumbHeight);
+    }
+
+    private addEventListeners() {
+        this.nodes.shifted.addEventListener('scroll', () => {
+            this.moveThumb();
+            this.executeScrollCallbacks();
+        });
+        this.nodes
+            .thumb.addEventListener('mousedown', (e) => this.startThumbDragging(e));
+    }
+
+    private moveThumb() {
+        let newTop = this.nodes.bar.offsetHeight * this.nodes.shifted.scrollTop
+            / this.nodes.shifted.scrollHeight;
+
+        this.nodes.thumb.style.setProperty('top', newTop + 'px');
+    }
+
+    private executeScrollCallbacks() {
+        let shifted = this.nodes.shifted;
+
+        this.scroll.callbacks.forEach((callback) => callback({
+            offsetHeight: shifted.offsetHeight,
+            scrollHeight: shifted.scrollHeight,
+            scrollTop: shifted.scrollTop,
+        }));
+    }
+
+    private startThumbDragging(e) {
+        this.nodes.el.classList.add('thumb-drugging');
+        this.scroll.thumbY = e.screenY;
+        this.addDocumentEventListener('mousemove', (e) => this.dragThumb(e));
+        this.addDocumentEventListener('mouseup', (e) => this.finishThumbDragging(e));
+        e.preventDefault();
+    }
+
+    private addDocumentEventListener(name, callback) {
+        this.document.listeners.push({
+            name: name,
+            callback: callback,
+        });
+
+        this.document.el.addEventListener(name, callback);
+    }
+
+    private dragThumb(e) {
+        let deltaY = (e.screenY - this.scroll.thumbY)
+            * this.nodes.shifted.scrollHeight / this.nodes.bar.offsetHeight;
+
+        this.scroll.thumbY = e.screenY;
+        this.nodes.shifted.scrollTop += deltaY;
+        e.preventDefault();
+    }
+
+    private finishThumbDragging(e) {
+        this.nodes.el.classList.remove('thumb-drugging');
+
+        this.document.listeners.forEach(
+            (listener) =>
+                this.document
+                    .el.removeEventListener(listener.name, listener.callback)
+        );
+
+        e.preventDefault();
     }
 }
